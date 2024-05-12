7a4172d173767098543d84110efaf160d03d9c3b
Biswajit Dey <biswajitdey.one@gmail.com>
2024-05-12 13:16:10 +0530

    chore: Update favicon to use brainwaveo.png instead of vite.svg

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
     }, []);
   return !isLoading ? (
     <>
-    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden" >
         
         <Header />
         <Hero />
